47087eff4f23c3d80ee4fe6bec581bfe13fe1224
absideu-tech <absideu@gmail.com>
2026-09-12 12:07:57 +0900

    22

--- time5.py
+++ time5.py
@@ -69,7 +69,6 @@ class TimeMasterApp:
         # 문구 라벨 제거 및 위아래 중앙 정렬 적용, padx 커지면 너비 좁아짐
             btn_container = tk.Frame(self.main_frame)
             btn_container.pack(expand=True, fill="x", padx=40)
-
         self.create_custom_button(
             btn_container, 
             "단순 계산", 
